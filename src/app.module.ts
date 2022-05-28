44b9705a9e5be8727ca006e64fcc2d87c43ed748
taechit chatthamporn <taechit.c@gmail.com>
2022-05-28 23:58:10 +0700

    init vehicle module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ParkingModule } from './parking/parking.module';
 import { ParkingSlotModule } from './parking-slot/parking-slot.module';
 import { TicketModule } from './ticket/ticket.module';
 import { ParkingFloorModule } from './parking-floor/parking-floor.module';
+import { VehicleModule } from './vehicle/vehicle.module';
 
 @Module({
-  imports: [ParkingModule, ParkingSlotModule, TicketModule, ParkingFloorModule],
+  imports: [ParkingModule, ParkingSlotModule, TicketModule, ParkingFloorModule, VehicleModule],
   controllers: [AppController],
   providers: [AppService],
 })
