fbac1d99bff13541b90c896699046829567ece37
taechit chatthamporn <taechit.c@gmail.com>
2022-05-28 23:36:19 +0700

    update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParkingModule } from './parking/parking.module';
+import { ParkingSlotModule } from './parking-slot/parking-slot.module';
+import { TicketModule } from './ticket/ticket.module';
+import { ParkingFloorModule } from './parking-floor/parking-floor.module';
 
 @Module({
-  imports: [],
+  imports: [ParkingModule, ParkingSlotModule, TicketModule, ParkingFloorModule],
   controllers: [AppController],
   providers: [AppService],
 })
