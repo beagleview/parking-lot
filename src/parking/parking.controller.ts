96c25faec6c3e17523c9572cf07dc4f142f2b86a
taechit chatthamporn <taechit.c@gmail.com>
2022-05-29 20:12:33 +0700

    create get and create api

--- src/parking/parking.controller.ts
+++ src/parking/parking.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ParkingRequest } from './dtos/parking-req.dtos';
+import { ParkingService } from './parking.service';
 
-@Controller('parking')
-export class ParkingController {}
+@Controller('admin')
+export class ParkingController {
+    constructor(private parkingService: ParkingService) { }
+
+    @Get('parkings')
+    findAllParking(){
+        return this.parkingService.findAll();
+    }
+
+    @Post('parkings')
+    createParking(@Body() req: ParkingRequest){
+        return this.parkingService.create(req);
+    }
+}
--- src/parking/parking.service.ts
+++ src/parking/parking.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ParkingRequest } from './dtos/parking-req.dtos';
+import { Parking } from './parking.entity';
 
 @Injectable()
-export class ParkingService {}
+export class ParkingService {
+    constructor(@InjectRepository(Parking) private parkingRepo: Repository<Parking>) { }
+
+    public async findAll(): Promise<Parking[]> {
+        const result = await this.parkingRepo.find({
+            order: { createdDateTime: "DESC" }
+        });
+        return result;
+    }
+    
+    public async create(req: ParkingRequest): Promise<Parking> {
+        const parking: Parking = await this.parkingRepo.create({ ...req });
+        return this.parkingRepo.save(parking);
+    }
+}
