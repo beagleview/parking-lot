1564cc025e6fee897fce7e74bb849afcae384927
taechit chatthamporn <taechit.c@gmail.com>
2022-05-29 20:36:07 +0700

    import floor module

--- src/parking/parking.module.ts
+++ src/parking/parking.module.ts
@@ -3,9 +3,10 @@ import { ParkingService } from './parking.service';
 import { ParkingController } from './parking.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Parking } from './parking.entity';
+import { ParkingFloorModule } from 'src/parking-floor/parking-floor.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Parking])],
+  imports:[TypeOrmModule.forFeature([Parking]), ParkingFloorModule],
   providers: [ParkingService],
   controllers: [ParkingController],
 })
