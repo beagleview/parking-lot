0b7d35ff7aa3ccb160fbc40485b1307776f93e54
taechit chatthamporn <taechit.c@gmail.com>
2022-05-29 20:38:03 +0700

    add floor api

--- src/parking/parking.controller.ts
+++ src/parking/parking.controller.ts
@@ -1,18 +1,33 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ParkingFloorReq } from 'src/parking-floor/dtos/parking-floor-req.dtos';
+import { ParkingFloorService } from '../parking-floor/parking-floor.service';
 import { ParkingRequest } from './dtos/parking-req.dtos';
 import { ParkingService } from './parking.service';
 
 @Controller('admin')
 export class ParkingController {
-    constructor(private parkingService: ParkingService) { }
+    constructor(
+        private parkingService: ParkingService,
+        private floorService: ParkingFloorService
+    ) { }
 
     @Get('parkings')
-    findAllParking(){
+    findAllParking() {
         return this.parkingService.findAll();
     }
 
     @Post('parkings')
-    createParking(@Body() req: ParkingRequest){
+    createParking(@Body() req: ParkingRequest) {
         return this.parkingService.create(req);
+    }
+
+    @Get('floors')
+    findAllFloor() {
+        return this.floorService.findAll();
+    }
+
+    @Post('floors')
+    createFloor(@Body() req: ParkingFloorReq) { 
+        return this.floorService.create(req);
     }
 }
