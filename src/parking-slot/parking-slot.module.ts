304d675af27c73718b9cd8f8864a9f14f375d90d
taechit chatthamporn <taechit.c@gmail.com>
2022-05-29 21:45:36 +0700

    import typeorm module

--- src/parking-slot/parking-slot.module.ts
+++ src/parking-slot/parking-slot.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ParkingSlot } from './parking-slot.entity';
 import { ParkingSlotService } from './parking-slot.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([ParkingSlot])],
   providers: [ParkingSlotService],
   exports:[ParkingSlotService]
 })
