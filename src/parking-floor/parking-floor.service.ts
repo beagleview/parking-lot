6e6169cb4d66c4126cb54ada1d14678c6f9cb35c
taechit chatthamporn <taechit.c@gmail.com>
2022-05-29 20:36:21 +0700

    update request

--- src/parking-floor/parking-floor.service.ts
+++ src/parking-floor/parking-floor.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ParkingFloorReq } from './dtos/parking-floor-req.dtos';
 import { ParkingFloor } from './parking-floor.entity';
 
 @Injectable()
 export class ParkingFloorService {
     constructor(@InjectRepository(ParkingFloor) private floorRepo: Repository<ParkingFloor>) { }
 
-    public async findAll(): Promise<ParkingFloor[]>{
+    public async findAll(): Promise<ParkingFloor[]> {
         const result = await this.floorRepo.find({
             order: { createdDateTime: "DESC" }
         });
         return result;
     }
 
-    public async create(req: any): Promise<ParkingFloor[]> {
-        const floor: ParkingFloor[] = await this.floorRepo.create({ ...req });
+    public async create(req: ParkingFloorReq): Promise<ParkingFloor> {
+        const floor: ParkingFloor = await this.floorRepo.create({ ...req });
         return this.floorRepo.save(floor);
     }
 }
