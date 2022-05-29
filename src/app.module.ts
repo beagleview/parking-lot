249292778ebcae6eb34c6cc32e85f48cc2106b06
taechit chatthamporn <taechit.c@gmail.com>
2022-05-29 16:58:04 +0700

    config connect database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { readFile } from 'fs/promises';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ParkingModule } from './parking/parking.module';
@@ -6,10 +10,36 @@ import { ParkingSlotModule } from './parking-slot/parking-slot.module';
 import { TicketModule } from './ticket/ticket.module';
 import { ParkingFloorModule } from './parking-floor/parking-floor.module';
 import { VehicleModule } from './vehicle/vehicle.module';
+import { Parking } from './parking/parking.entity';
+import { ParkingFloor } from './parking-floor/parking-floor.entity';
 
 @Module({
-  imports: [ParkingModule, ParkingSlotModule, TicketModule, ParkingFloorModule, VehicleModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          database: config.get<string>('DB_NAME'),
+          port: Number(config.get<string>('DB_PORT')),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          entities: [Parking, ParkingFloor],
+          ssl: {
+            rejectUnauthorized: false,
+            ca: readFile('cert/pg-db.crt').toString(),
+          },
+          synchronize: true,
+        };
+      }
+    }),
+    ParkingModule, ParkingSlotModule, TicketModule, ParkingFloorModule, VehicleModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
