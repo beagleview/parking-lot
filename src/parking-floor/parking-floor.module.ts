11e3f27892e6191c6b02d814b86b0b591fdf80fc
taechit chatthamporn <taechit.c@gmail.com>
2022-05-29 20:13:06 +0700

    add creat get and create floor

--- src/parking-floor/parking-floor.module.ts
+++ src/parking-floor/parking-floor.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ParkingFloor } from './parking-floor.entity';
 import { ParkingFloorService } from './parking-floor.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ParkingFloor])],
   providers: [ParkingFloorService],
-  exports:[ParkingFloorService]
+  exports: [ParkingFloorService]
 })
-export class ParkingFloorModule {}
+export class ParkingFloorModule { }
--- src/parking-floor/parking-floor.service.ts
+++ src/parking-floor/parking-floor.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ParkingFloor } from './parking-floor.entity';
 
 @Injectable()
-export class ParkingFloorService {}
+export class ParkingFloorService {
+    constructor(@InjectRepository(ParkingFloor) private floorRepo: Repository<ParkingFloor>) { }
+
+    public async findAll(): Promise<ParkingFloor[]>{
+        const result = await this.floorRepo.find({
+            order: { createdDateTime: "DESC" }
+        });
+        return result;
+    }
+
+    public async create(req: any): Promise<ParkingFloor[]> {
+        const floor: ParkingFloor[] = await this.floorRepo.create({ ...req });
+        return this.floorRepo.save(floor);
+    }
+}
